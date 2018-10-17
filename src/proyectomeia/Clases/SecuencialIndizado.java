dcb7771feb6e9a720c30424c2865896228b98233
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-10-17 16:44:22 -0600

    Modificando metodos

--- src/proyectomeia/Clases/SecuencialIndizado.java
+++ src/proyectomeia/Clases/SecuencialIndizado.java
@@ -51,63 +51,15 @@ public class SecuencialIndizado {
         maxregistros = Maxregistros;
     }
     public void Insertar(String value) {
-        try {
-            Archivo = new RandomAccessFile(masterfile, "rw");
-            long size = Archivo.length();
-            Archivo.seek(size);
-            Archivo.writeBytes(value);
-            Archivo.writeBytes(System.lineSeparator());
-            Archivo.close();
-            //Falta actualización del descriptor
-            UpdateDescriptor(String.valueOf(RegistrosActivos()),String.valueOf(RegistrosInactivos()),new SimpleDateFormat("yyyyMMdd.HH:mm").format(Calendar.getInstance().getTime()));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ApiloFile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ApiloFile.class.getName()).log(Level.SEVERE, null, ex);
-        }
+       
 
     }
     public int getNoRegistros(){
-        registros = 0;
-        try {
-            Archivo = new RandomAccessFile(masterfile,"r");
-            String linea;
-            while((linea = Archivo.readLine())!= null){
-                if(!linea.contains("/0"))
-                        registros++;
-            }
-            Archivo.close();
-            
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ApiloFile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ApiloFile.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return registros;
+        
+        return 0;
     }
     public List<String> Busqueda(String value){
-        List<String> found = new ArrayList<>();
-        try {
-            Archivo = new RandomAccessFile(masterfile, "r");
-            String line;
-            int busqueda = 0;
-            for(int j = 0; j < atributos.length;j++){
-                if(llave[0].equals(atributos[j])){
-                    busqueda = j;
-                }
-            }
-            while((line = Archivo.readLine())!= null){
-                   String compare = line.split("\\|")[busqueda];
-                   if(compare.trim().equals(value.trim()))
-                           found.add(line);
-               }    
-            Archivo.close();
-            
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ApiloFile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ApiloFile.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        List<String> found = new ArrayList<>();        
         
         return found;
     }
@@ -188,150 +140,36 @@ public class SecuencialIndizado {
         return posicion;
     }
     protected void EliminacionLogica(String registro) throws FileNotFoundException, IOException{
-        int position = PosicionRegistro(registro);
-        Archivo = new RandomAccessFile(masterfile,"rw");
-        for(int i = -1; i<position; i++){
-            Archivo.readLine();
-        }
-        long rewriting = Archivo.getFilePointer();
-        Archivo.seek(rewriting);
-        String registroEliminado = registro.substring(0, registro.length()-2)+"/0";
-        Archivo.writeBytes(registroEliminado);
-        Archivo.close();
-        UpdateDescriptor(String.valueOf(RegistrosActivos()),String.valueOf(RegistrosInactivos()),new SimpleDateFormat("yyyyMMdd.HH:mm").format(Calendar.getInstance().getTime()));
+        
     }
     protected int RegistrosActivos() throws FileNotFoundException, IOException{
-     Archivo = new RandomAccessFile(masterfile,"r");
-     String linea;
-     int records = 0;
-     while((linea = Archivo.readLine())!=null){
-         String[]check = linea.split("\\|");
-     
-         if(check[check.length-1].contains("1")&&!check[check.length-1].contains("/0")){
-             records++;
-         }
-       }
-     Archivo.close();
-    
-        return records;
+     return 0;
     }
     protected int RegistrosInactivos() throws FileNotFoundException, IOException{
-     Archivo = new RandomAccessFile(masterfile,"r");
-     int registers = 0;
-     String linea;
-     while((linea = Archivo.readLine())!= null){
-         String[] check = linea.split("\\|");
-         if(check[check.length-1].contains("0")&&!check[check.length-1].contains("/0")){
-             registers++;
-         }
-     }
-     Archivo.close();
-        return registers;
+     
+        return 0;
     }
     public void updateAutor(String autor) throws FileNotFoundException, IOException{
-        Archivo = new RandomAccessFile(descriptor, "rw");
-        String linea;
-        long bytestosave = 0;
-        while((linea = Archivo.readLine())!= null){
-            
-           if(linea.contains("Organización")){
-                bytestosave = Archivo.getFilePointer();
-                Archivo.seek(bytestosave);
-                String newvalue = "Autor:" + rightpad(autor,20);
-               Archivo.writeBytes(newvalue);
-               Archivo.close();
-                return; 
-            }
-           
-           
-            
-        }
         
         
     }
      protected void UpdateDescriptor(String registrosA, String registrosI, String fechamod) throws FileNotFoundException, IOException{
-      Archivo = new RandomAccessFile(descriptor,"rw");
-      String linea;
-       while((linea = Archivo.readLine())!= null){
-            if(linea.contains("Creado")){
-                long mod = Archivo.getFilePointer();
-                Archivo.seek(mod);
-                String newsvalue = "Modificado:" + fechamod;
-                Archivo.writeBytes(newsvalue);
-            } 
-       }
-        linea = null;
-        Archivo.close();
-        Archivo = null;
-        Archivo = new RandomAccessFile(descriptor,"rw");
-        while((linea = Archivo.readLine())!= null){
-           if(linea.contains("Separador de Campos")){
-               long aregis = Archivo.getFilePointer();
-               Archivo.seek(aregis);
-                String replace = "Registros Activos:" + rightpad(registrosA,6);
-                Archivo.writeBytes(replace);
-            } 
-        }
-        linea = null;
-        Archivo.close();
-        Archivo = null;
-        Archivo = new RandomAccessFile(descriptor,"rw");
-        while((linea = Archivo.readLine())!= null){
-             if(linea.contains("Registros Activos")){
-                 long iregs = Archivo.getFilePointer();
-                 Archivo.seek(iregs);
-                String newvalue = "Registros Inactivos:" + rightpad(registrosI,6);
-                Archivo.writeBytes(newvalue);
-            }
-        }
+     
       
     }
-    public void Reorganizar() throws FileNotFoundException, IOException{   
-     Archivo = new RandomAccessFile(masterfile,"r");
-     Queue<String> refactor = new LinkedList<>();
-     String keepath = masterfile.getPath();
-          String linea;
-          while((linea = Archivo.readLine())!= null){
-              if(!linea.contains(("/0"))){
-                  refactor.add(linea);
-              }
-          }
-          Archivo.close();
-          masterfile.delete();
-       File tempfile = new File(keepath); 
-      Archivo = new RandomAccessFile(tempfile,"rw");
-      String decoy;
-      decoy = refactor.poll();
-        while(decoy != null){
-          Archivo.writeBytes(decoy);
-          Archivo.writeBytes(System.lineSeparator());
-          decoy = refactor.poll();
-      }
-      Archivo.close();
-      masterfile = tempfile;
+    public void Reorganizar() throws FileNotFoundException, IOException{  
+     
       
         
     }
     public void empty() throws IOException{
-        FileChannel.open(Paths.get(masterfile.getPath()), StandardOpenOption.WRITE).truncate(0).close();
-        UpdateDescriptor("0","0",new SimpleDateFormat("yyyyMMdd.HH:mm").format(Calendar.getInstance().getTime()));
+        
     }
      protected String rightpad(String text, int length) {
     return String.format("%-" + length + "." + length + "s", text);
 }
     public int ReturnMaxreg() throws FileNotFoundException, IOException{
-        String linea;
-        int result = 0;
-        RandomAccessFile archive = new RandomAccessFile(descriptor,"rw");
-          while((linea = archive.readLine())!= null){
-             if(linea.contains("Registros Inactivos")){
-                 long iregs = archive.getFilePointer();
-                 archive.seek(iregs);
-                String score = archive.readLine();
-                
-                
-            }
-        }
-          return result;
+        
+          return 0;
     }
 }
